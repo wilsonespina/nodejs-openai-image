2ac4af86c8a9108743213726a08b667852a53cdb
wilson espina <wilson.espina@gmail.com>
2023-04-22 17:05:36 +1200

    feat: adds parcel scripts

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+console.log("=======🚀 ~ file: index.js:5 ~ port:", port)
+
 const app = express();
+
+console.log('INDEX FILE')
 
 // Enable body parser
 app.use(express.json());
@@ -12,6 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/openai', require('./routes/openaiRoutes'));
+app.use('/openai', require('./public/routes/openaiRoutes'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
