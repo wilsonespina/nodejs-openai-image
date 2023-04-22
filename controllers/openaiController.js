67efa5b40c5c0b4b4efb6a887c00259b189e27de
wilson espina <wilson.espina@gmail.com>
2023-04-22 13:19:52 +1200

    chore: adds dot env require statment:

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,7 +1,10 @@
 const { Configuration, OpenAIApi } = require('openai');
+const dotenv = require('dotenv').config();
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
